3b0f39fc521f0f163ff0bf2830e1a82dd8fab9fb
Alex Li <alexlioralexli@gmail.com>
2021-01-14 03:53:18 -0800

    fixed typo

--- experiment_scripts/1-13-2021/1-13-2021.py
+++ experiment_scripts/1-13-2021/1-13-2021.py
@@ -3,7 +3,7 @@ count = 0
 envs = ['Ant-v3', 'HalfCheetah-v3']
 fair_first_widths = [869, 882]
 for env, first_dim in zip(envs, fair_first_widths):
-    for depth in ['--n_hidden 2 --exp 12', '--n_hidden 3 --exp 13']:
+    for depth in ['--n_hidden 2 --expID 12', '--n_hidden 3 --expID 13']:
         for seed in [10, 20, 30, 40, 50]:
             print(f'CUDA_VISIBLE_DEVICES={count % 5} python main.py --env {env} --first_dim {first_dim} {depth} --seed {seed} &')
             count += 1
