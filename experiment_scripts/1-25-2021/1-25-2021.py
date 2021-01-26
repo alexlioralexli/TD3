90d809cd34ef64a90cb09bee94f00144d43fbafd
Alex Li <alexlioralexli@gmail.com>
2021-01-26 01:54:23 -0800

    experiment scripts

---
+++ experiment_scripts/1-25-2021/1-25-2021.py
@@ -0,0 +1,35 @@
+# --expID 27
+envs = [
+'dm.acrobot.swingup',
+'dm.cheetah.run',
+'dm.finger.turn_hard',
+'dm.fish.swim',
+'dm.hopper.hop',
+'dm.quadruped.run',
+'dm.quadruped.walk',
+'dm.swimmer.swimmer15',
+'dm.swimmer.swimmer6',
+'dm.walker.run',
+'dm.humanoid.run',
+'dm.humanoid.stand',
+'dm.humanoid.walk',
+]
+total = 0
+for env in envs:
+    base_str = f"python main.py --policy PytorchSAC --env {env} --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2"
+    commands = [base_str,
+                f"python main.py --policy PytorchSAC --env {env} --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 3"]
+    # fourier features
+    for type in ['--train_B', '--concatenate_fourier --train_B']:
+        for sigma in [0.01, 0.001]:
+            commands.append(base_str + f' --network_class FourierMLP --sigma {sigma} --fourier_dim 1024 {type}')
+    count = 0
+    for command in commands:
+        # gpus = list(range(8,10))
+        gpus = list(range(10))
+        for seed in [10, 20]:
+            if total % 8 == 0:
+                print(total)
+            total += 1
+            print(f'CUDA_VISIBLE_DEVICES={gpus[count]} {command} --seed {seed} &')
+            count = (count + 1) % len(gpus)
---
+++ experiment_scripts/1-25-2021/gargouille.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+CUDA_VISIBLE_DEVICES=5 python main.py --policy PytorchSAC --env dm.swimmer.swimmer15 --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2 --network_class FourierMLP --sigma 0.01 --fourier_dim 1024 --concatenate_fourier --train_B --seed 10 &
+CUDA_VISIBLE_DEVICES=6 python main.py --policy PytorchSAC --env dm.swimmer.swimmer15 --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2 --network_class FourierMLP --sigma 0.01 --fourier_dim 1024 --concatenate_fourier --train_B --seed 20 &
+CUDA_VISIBLE_DEVICES=7 python main.py --policy PytorchSAC --env dm.swimmer.swimmer15 --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2 --network_class FourierMLP --sigma 0.001 --fourier_dim 1024 --concatenate_fourier --train_B --seed 10 &
+CUDA_VISIBLE_DEVICES=8 python main.py --policy PytorchSAC --env dm.swimmer.swimmer15 --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2 --network_class FourierMLP --sigma 0.001 --fourier_dim 1024 --concatenate_fourier --train_B --seed 20 &
+CUDA_VISIBLE_DEVICES=9 python main.py --policy PytorchSAC --env dm.swimmer.swimmer6 --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2 --seed 10 &
+CUDA_VISIBLE_DEVICES=5 python main.py --policy PytorchSAC --env dm.swimmer.swimmer6 --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2 --seed 20 &
+CUDA_VISIBLE_DEVICES=6 python main.py --policy PytorchSAC --env dm.swimmer.swimmer6 --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 3 --seed 10 &
+CUDA_VISIBLE_DEVICES=7 python main.py --policy PytorchSAC --env dm.swimmer.swimmer6 --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 3 --seed 20 &
+CUDA_VISIBLE_DEVICES=8 python main.py --policy PytorchSAC --env dm.swimmer.swimmer6 --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2 --network_class FourierMLP --sigma 0.01 --fourier_dim 1024 --train_B --seed 10 &
+CUDA_VISIBLE_DEVICES=9 python main.py --policy PytorchSAC --env dm.swimmer.swimmer6 --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2 --network_class FourierMLP --sigma 0.01 --fourier_dim 1024 --train_B --seed 20 &
+CUDA_VISIBLE_DEVICES=8 python main.py --policy PytorchSAC --env dm.swimmer.swimmer6 --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2 --network_class FourierMLP --sigma 0.001 --fourier_dim 1024 --train_B --seed 10 &
+CUDA_VISIBLE_DEVICES=9 python main.py --policy PytorchSAC --env dm.swimmer.swimmer6 --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2 --network_class FourierMLP --sigma 0.001 --fourier_dim 1024 --train_B --seed 20 &
