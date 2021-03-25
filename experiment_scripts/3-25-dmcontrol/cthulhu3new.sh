1aa9fb1493a848abb940bb072f361d0932af83aa
Alex Li <alexlioralexli@gmail.com>
2021-03-25 14:58:13 -0700

    reduce fourier dim for finger so i avoid overflow

--- experiment_scripts/3-25-dmcontrol/cthulhu3new.sh
+++ experiment_scripts/3-25-dmcontrol/cthulhu3new.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=6 taskset -c 0-2 python main.py --policy PytorchSAC --env dm.finger.turn_hard --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2 --lr 1e-4 --network_class LogUniformFourierMLP --fourier_dim 1024  --seed 10 &
+CUDA_VISIBLE_DEVICES=6 taskset -c 0-2 python main.py --policy PytorchSAC --env dm.finger.turn_hard --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2 --lr 1e-4 --network_class LogUniformFourierMLP --fourier_dim 256 --seed 10 &
 CUDA_VISIBLE_DEVICES=7 taskset -c 3-5 python main.py --policy PytorchSAC --env dm.walker.run --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2 --lr 1e-4 --network_class LogUniformFourierMLP --fourier_dim 1024 --concatenate_fourier --train_B --seed 10 &
 CUDA_VISIBLE_DEVICES=6 taskset -c 6-8 python main.py --policy PytorchSAC --env dm.walker.run --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2 --lr 1e-4 --network_class LogUniformFourierMLP --fourier_dim 1024 --concatenate_fourier --seed 10 &
 CUDA_VISIBLE_DEVICES=7 taskset -c 9-11 python main.py --policy PytorchSAC --env dm.walker.run --start_timesteps 5000 --hidden_dim 1024 --batch_size 1024 --n_hidden 2 --lr 1e-4 --network_class LogUniformFourierMLP --fourier_dim 1024 --train_B --seed 10 &
